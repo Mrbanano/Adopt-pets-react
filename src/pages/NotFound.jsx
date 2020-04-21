264523f8b73dad5ad95b28e640a1be0af1302fa8
Alvaro <6666alvaro666@gmail.com>
2020-04-21 13:47:33 -0500

    404

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+function NotFound() {
+    return (
+        <div className="NotFound">
+            <div className="NotFound-container">
+                <div className="NotFound-Container">
+                    <h3>Ups.... algo salio mal </h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NotFound;
