8cd92479b59c52809c8183a5f345d095c7410641
Alvaro <6666alvaro666@gmail.com>
2020-04-21 12:50:03 -0500

    css add

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,13 @@ module.exports = {
             loader: "html-loader"
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
       }
     ]
   },
