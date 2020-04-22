71cc90cf0700b24d90361ee4970fac25a26ccc2c
Alvaro <6666alvaro666@gmail.com>
2020-04-22 11:44:16 -0500

    facebook login logout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import {auth , provider} from '../utils/firebase';
+
 function Login() {
+
+    const loginFacebook = ()=>{
+        auth().signInWithPopup(provider)
+          .then(({ user })=>{
+            console.log(user);
+          });
+    }
+    
+    const logoutFacebook = ()=>{
+        auth().signOut()
+          .then(({})=>{
+              console.log("cerrar session");
+          });
+    }
+
     return (
         <div className="Login">
             <div className="Login-container">
                 <div className="Login-content">
                     <h2>Crear cuenta o iniciar seccion</h2>
                 </div>
-                <button>
-                    <i>logo </i>
-                    <span>Inicar seccion con fb </span>
+                <button onClick={loginFacebook}>
+                    <i className="fab fa-facebook-square"></i>
+                    <span> Inicar seccion con fb </span>
+                </button>
+                <button onClick={logoutFacebook}>
+                    Cerrar ssession
                 </button>
             </div>
         </div>
